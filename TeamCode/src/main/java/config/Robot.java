947afced7dffb11f5bae6d7e72629c4df0289afb
danton <dantonmel8@gmail.com>
2025-08-25 22:19:17 -0300

    Op Mode simplificado

--- TeamCode/src/main/java/config/Robot.java
+++ TeamCode/src/main/java/config/Robot.java
@@ -1,17 +1,10 @@
 package config;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-
-
-import static robot.Subsystems.Arm.arm;
-import static robot.Subsystems.Extend.extend;
-
 import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import pedro.constants.FConstants;
 import pedro.constants.LConstants;
@@ -22,20 +15,25 @@ import robot.Subsystems.Extend;
 import robot.Subsystems.Initialize;
 
 public class Robot {
-    public  Follower f;
-    private  HardwareMap h;
+    public Follower f;
+    public HardwareMap h;
+    public Telemetry t;
+    public  Gamepad g1, g2;
     boolean controleManualBraco, controleManualLinear;
-    private Initialize i;
+    public Initialize i;
     public Pose autoEndPose;
 
     public static Pose autoEnd = new Pose(60, 100, Math.toRadians(90));
 
-    public Robot(HardwareMap h, Pose autoEndPose) {
-        this.autoEndPose = autoEndPose;
+    public Robot(HardwareMap h, Telemetry t, Gamepad g1, Gamepad g2, Pose autoEndPose) {
         this.h = h;
-        f = new Follower(hardwareMap, FConstants.class, LConstants.class);
-        i = new Initialize (hardwareMap);
+        this.t = t;
+        this.g1 = g1;
+        this.g2 = g2;
+        this.autoEndPose = autoEndPose;
 
+        f = new Follower(h, FConstants.class, LConstants.class);
+        i = new Initialize(h);
     }
 
     public void rStart() {
@@ -43,7 +41,6 @@ public class Robot {
     }
 
     public void dualControls() {
-
         drive();
         controlarBraco();
         controlarLinear();
@@ -51,60 +48,58 @@ public class Robot {
         controlarAngulo();
         atualizarTelemetria();
     }
+
     private void controlarBraco() {
-
         if (controleManualBraco) {
-            double potencia = -gamepad2.left_stick_y;
+            double potencia = -g2.left_stick_y;
             Arm.controlManual(potencia);
-
         }
     }
 
     private void controlarLinear() {
-
         if (controleManualLinear) {
-            double potencia = -gamepad2.right_stick_y;
+            double potencia = -g2.right_stick_y;
             Extend.setManualPower(potencia);
         }
 
-            if (gamepad2.dpad_right) {
-                Extend.retract();
-            }
-            if (gamepad2.dpad_left) {
-                Extend.extend();
-            }
-
+        if (g2.dpad_right) {
+            Extend.retract();
+        }
+        if (g2.dpad_left) {
+            Extend.extend();
+        }
     }
 
     private void controlarGarra() {
-        if (gamepad2.right_trigger > 0.1) {
+        if (g2.right_trigger > 0.1) {
             Claw.open();
-        } if (gamepad2.left_trigger > 0.1) {
+        }
+        if (g2.left_trigger > 0.1) {
             Claw.close();
         }
     }
 
     private void controlarAngulo() {
-        if (gamepad2.left_bumper) {
+        if (g2.left_bumper) {
             Angle.up();
-        }if (gamepad2.right_bumper) {
+        }
+        if (g2.right_bumper) {
             Angle.down();
         }
     }
+
     private void atualizarTelemetria() {
-        telemetry.addData("Modo Braço", controleManualBraco ? "MANUAL" : "AUTOMÁTICO");
-        telemetry.addData("Posição Braço", arm.getCurrentPosition());
-        telemetry.addData("Posição Linear", extend.getCurrentPosition());
-        telemetry.addData("Modo Linear", controleManualLinear ? "MANUAL" : "AUTOMÁTICO");
-        telemetry.update();
+        t.addData("Modo Braço", controleManualBraco ? "MANUAL" : "AUTOMÁTICO");
+        t.addData("Posição Braço", Arm.arm.getCurrentPosition());
+        t.addData("Posição Linear", Extend.extend.getCurrentPosition());
+        t.addData("Modo Linear", controleManualLinear ? "MANUAL" : "AUTOMÁTICO");
+        t.update();
     }
-    public void drive (){
-        double velocidade = (gamepad1.right_trigger>0.1) ? 0.5 : 1.0;
-        f.setTeleOpMovementVectors(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x, true);
+
+    public void drive() {
+        double velocidade = (g1.right_trigger > 0.1) ? 0.5 : 1.0;
+        f.setTeleOpMovementVectors(-g1.left_stick_y, -g1.left_stick_x, -g1.right_stick_x, true);
         f.update();
         f.setMaxPower(velocidade);
     }
-
-
 }
-
